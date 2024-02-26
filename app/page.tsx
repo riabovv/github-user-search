e589aeab24bd877133c5bd38e1b99dcc4e0f11cd
Vadym <flypilott74@gmail.com>
2024-02-26 14:25:16 +0100

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
+import Footer from "./components/layout/Footer/Footer";
 
 const Home = () => (
   <>
     <Header />
     <Main />
-    <footer></footer>
+    <Footer />
   </>
 );
 
