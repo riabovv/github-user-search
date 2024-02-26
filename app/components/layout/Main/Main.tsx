585b80709ed363520b6b8939edf8e47c4441be2b
Vadym <flypilott74@gmail.com>
2024-02-26 13:37:10 +0100

    add link to Github and hover styling

--- app/components/layout/Main/Main.tsx
+++ app/components/layout/Main/Main.tsx
@@ -6,7 +6,15 @@ const Main = () => {
     <main className={styles.wrapper}>
       <div className={styles.searchWrapper}>
         <span className={styles.title}>
-          Search for any GitHub user you want to find
+          Search for any{" "}
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          user you want to find
         </span>
         <MainForm />
       </div>
