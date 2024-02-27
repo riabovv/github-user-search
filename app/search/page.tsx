1f778d89415e90a366d35e4d00d0efb89443bd06
Vadym <flypilott74@gmail.com>
2024-02-27 21:30:45 +0100

    add search route

---
+++ app/search/page.tsx
@@ -0,0 +1,22 @@
+// "use client";
+
+// import { useSearchParams } from "next/navigation";
+
+// const Search = () => {
+//   const searchParams = useSearchParams();
+//   const hasQueryParam = !!searchParams.get("q");
+
+//   console.log(hasQueryParam);
+
+//   return (
+//     <>
+//       <span>Search</span>
+//     </>
+//   );
+// };
+
+// export default Search;
+
+const Search = () => <span>Search</span>;
+
+export default Search;
