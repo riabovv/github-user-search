d175cc235b296e2df6072a5b02de03064b1355f2
Vadym <flypilott74@gmail.com>
2024-03-25 18:22:26 +0100

    add google verification tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   title: "Github User Search",
   description:
     "Discover GitHub users effortlessly with Github User Search – the ultimate GitHub user search app. Explore profiles, repositories, and stay connected with the open-source community. Simple, fast, and secure. Optimize your GitHub experience today!",
+  verification: { google: "Q7Z2jzySGE-fFLtIRF-NFG_kdxvyVi3PAUVe0_9UFwg" },
 };
 
 const RootLayout = ({
