691d1d0f9bda90690a90ec1bd773eb5aa717dbf4
Vadym <flypilott74@gmail.com>
2024-05-22 13:57:59 +0200

    add source code link

--- app/components/layout/Footer/Footer.tsx
+++ app/components/layout/Footer/Footer.tsx
@@ -2,16 +2,29 @@ import styles from "./footer.module.css";
 
 const Footer = () => (
   <footer className={styles.wrapper}>
-    <i className={styles.title}>
-      Developed by{" "}
-      <a
-        href="https://github.com/riabovv"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        riabovv
-      </a>
-    </i>
+    <div>
+      <i className={styles.text}>
+        Developed by{" "}
+        <a
+          href="https://github.com/riabovv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          riabovv
+        </a>
+      </i>
+    </div>
+    <div>
+      <i className={styles.text}>
+        <a
+          href="https://github.com/riabovv/github-user-search"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View the source code
+        </a>
+      </i>
+    </div>
   </footer>
 );
 
